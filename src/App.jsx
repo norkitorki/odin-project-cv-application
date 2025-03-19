2a1ae91381f4008555e6f95510fd169d10872774
norkitorki <norki@tuta.io>
2025-03-19 11:07:04 +0100

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Personal from './components/Personal';
+import Education from './components/Education';
+import Practical from './components/Practical';
+
+export default function App() {
+  const [isEditing, setIsEditing] = useState(true);
+
+  return (
+    <>
+      <header className="no-print">
+        <nav>
+          <button onClick={() => setIsEditing(!isEditing)}>
+            📝 {`${isEditing ? 'Save' : 'Edit'} Information`}
+          </button>
+          <button
+            onClick={() => {
+              setIsEditing(false);
+              setTimeout(() => print(), 1);
+            }}
+          >
+            📜 Print
+          </button>
+        </nav>
+      </header>
+      <h1>CV Application</h1>
+      <main>
+        <Personal isEditing={isEditing} />
+        <Education isEditing={isEditing} />
+        <Practical isEditing={isEditing} />
+      </main>
+    </>
+  );
+}
