fe111fd08e44fcb600f833d4d0ce1d59bf00ad62
norkitorki <norki@tuta.io>
2025-03-20 08:08:36 +0100

    Display personal section only if personal information is provided

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -21,45 +21,56 @@ function Input({ type, value, isEditing, onChange }) {
 
 export default function Personal({ isEditing }) {
   const [person, setPerson] = useState({
-    name: 'John Doe',
-    email: 'johndoe@mail.com',
-    phone: '353254643',
+    name: '',
+    email: '',
+    phone: '',
   });
+  const personIsEmpty = Object.values(person).join('') === '';
 
   const update = (event, property) => {
     setPerson({ ...person, [property]: event.target.value });
   };
 
   return (
-    <section className="personal">
-      <h2>Personal</h2>
-      <label>
-        Name
-        <Input
-          type="text"
-          value={person.name}
-          isEditing={isEditing}
-          onChange={(e) => update(e, 'name')}
-        />
-      </label>
-      <label>
-        Email
-        <Input
-          type="email"
-          value={person.email}
-          isEditing={isEditing}
-          onChange={(e) => update(e, 'email')}
-        />
-      </label>
-      <label>
-        Phone
-        <Input
-          type="tel"
-          value={person.phone}
-          isEditing={isEditing}
-          onChange={(e) => update(e, 'phone')}
-        />
-      </label>
-    </section>
+    <>
+      {(isEditing || !personIsEmpty) && (
+        <section className="personal">
+          <h2>Personal</h2>
+          {(isEditing || (!isEditing && person.name)) && (
+            <label>
+              Name
+              <Input
+                type="text"
+                value={person.name}
+                isEditing={isEditing}
+                onChange={(e) => update(e, 'name')}
+              />
+            </label>
+          )}
+          {(isEditing || (!isEditing && person.email)) && (
+            <label>
+              Email
+              <Input
+                type="email"
+                value={person.email}
+                isEditing={isEditing}
+                onChange={(e) => update(e, 'email')}
+              />
+            </label>
+          )}
+          {(isEditing || (!isEditing && person.phone)) && (
+            <label>
+              Phone
+              <Input
+                type="tel"
+                value={person.phone}
+                isEditing={isEditing}
+                onChange={(e) => update(e, 'phone')}
+              />
+            </label>
+          )}
+        </section>
+      )}
+    </>
   );
 }
